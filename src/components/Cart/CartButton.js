14f9baf08a8896572578f1a809781d521eeb63ad
Tojo Antonio ANDRIAMABGA <a.tojo.antonio@gmail.com>
2021-10-24 01:56:02 +0200

    manage cart visibility state

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
-import classes from './CartButton.module.css';
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+import { useDispatch } from "react-redux";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toogle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
